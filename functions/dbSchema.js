b7153e0628007004290ef77934d866b1771ce0f5
Ahmad Ali <aallii300300@gmail.com>
2020-02-21 22:25:00 +0000

    scream route impelmented

---
+++ functions/dbSchema.js
@@ -0,0 +1,11 @@
+let db ={
+    screams:[
+        {
+            userHandle: 'user',
+            body: 'scream body',
+            createdAt: "2020-02-21T21:56:25.955Z",
+            likeCount: 5,
+            commentCCount: 5
+        }
+    ]
+}
--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,56 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 
+/*
+* Initialize Firebase App and firestore DB
+*/
 admin.initializeApp();
 
 let db = admin.firestore();
 
+/*
+* Firebase App Configurations
+*/
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB9RaIZ4Tb3wQq_fNYJqf7UMlAUOGCSdgI",
+    authDomain: "socialapp-2bf83.firebaseapp.com",
+    databaseURL: "https://socialapp-2bf83.firebaseio.com",
+    projectId: "socialapp-2bf83",
+    storageBucket: "socialapp-2bf83.appspot.com",
+    messagingSenderId: "358200962945",
+    appId: "1:358200962945:web:bbcdffc621e54ec125c280",
+    measurementId: "G-BDN25F9NY0"
+  };
+
+/*
+* Initialize Express App
+*/
+
 const express = require('express');
 var app = express();
 
+/*
+* Initialize Firebase App
+*/
+const firebase = require('firebase');
+firebase.initializeApp(firebaseConfig);
+
+
+/*
+* Get Screams
+*/
+
 app.get('/screams', (request, response) => {
-    db.collection('screams').get().then((snapshot) => {
+    db.collection('screams').orderBy('createdAt', 'desc').get().then((snapshot) => {
         let screams = [];
         snapshot.forEach((doc) => {
-          console.log(doc.id, '=>', doc.data());
-          screams.push(doc.data());
+          screams.push({
+              id: doc.id,
+              body: doc.data().body,
+              userHandle: doc.data().userHandle,
+              createdAt: doc.data().createdAt
+          });
         });
         return response.json(screams);
       })
@@ -23,24 +60,52 @@ app.get('/screams', (request, response) => {
 
    });
 
+/*
+* Post a new Scream
+*/
 
+app.post('/screams', (request, response) => {
+    let newScream = {
+        body: request.body.body,
+        createdAt:new Date().toISOString(),
+        userHandle: request.body.userHandle
+    };
 
-   exports.createScreams = functions.https.onRequest((request, response) => {
-        let newScream = {
-            body: request.body.body,
-            createdAt:admin.firestore.Timestamp.fromDate(new Date()),
-            userHandle: request.body.userHandle
-        };
+    db.collection('screams').add(newScream).then((doc)=>{
+        response.json({ message: `Document created => ${ doc.id }` })
+    })
+    .catch((err) => {
+        console.log('Error getting documents', err);
+        response.status(500).json({"err": "Somthing went Wrong"})
+    });
+    
 
-        db.collection('screams').add(newScream).then((doc)=>{
-            response.json({ message: `Document created => ${ doc.data() }` })
-        })
-        .catch((err) => {
-            console.log('Error getting documents', err);
-            response.status(500).json({"err": "Somthing went Wrong"})
-        });
-        
+});
 
-   });
+/*
+* sign up route
+*/
 
-   exports.api = functions.https.onRequest(app);
+app.post('/signup', (req, res)=> {
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        confirmPassword: req.body.confirmPassword,
+        handle: req.body.handle,
+    }
+
+    firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password).then((data)=>{
+        res.status(201).json({message: `User ${data.user.uid} Created Successfully`})
+    }) 
+    .catch((err) => {
+        console.log('Error getting documents', err);
+        response.status(500).json({"error": err.code})
+    });
+});
+ 
+
+/*
+* Exporting /api route to handle other back-end functions
+*/
+
+exports.api = functions.region("europe-west1").https.onRequest(app);
