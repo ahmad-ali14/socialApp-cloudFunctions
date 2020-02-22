ad965c3a53986ff3ff074119098dafd39a952458
Ahmad Ali <aallii300300@gmail.com>
2020-02-22 13:55:06 +0000

    login route implemnted

--- functions/dbSchema.js
+++ functions/dbSchema.js
@@ -7,5 +7,18 @@ let db ={
             likeCount: 5,
             commentCCount: 5
         }
+    ],
+    users:[
+        {
+            userId: 'realjlfdbjldfb',
+            email: 'e@m.com',
+            handle: 'user',
+            createdAt: "2020-02-21T21:56:25.955Z",
+            imageUrl: 'https://fdkjdl..',
+            bio: 'user\'s bio',
+            website: 'ahmad-ali.co.uk',
+            location: 'London, UK'
+
+        }
     ]
 }
--- functions/index.js
+++ functions/index.js
@@ -18,7 +18,7 @@ const firebaseConfig = {
     authDomain: "socialapp-2bf83.firebaseapp.com",
     databaseURL: "https://socialapp-2bf83.firebaseio.com",
     projectId: "socialapp-2bf83",
-    storageBucket: "socialapp-2bf83.appspot.com",
+    storageBucket: process.env.STORAGE_BUCKET,
     messagingSenderId: "358200962945",
     appId: "1:358200962945:web:bbcdffc621e54ec125c280",
     measurementId: "G-BDN25F9NY0"
@@ -37,12 +37,47 @@ var app = express();
 const firebase = require('firebase');
 firebase.initializeApp(firebaseConfig);
 
+/*
+* Authentication meddillware to check if user logged in
+*/
+
+const FBauth = (req, res, next)=>{
+    //retrive token from request
+    let idToken;
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer ')){
+        idToken = req.headers.authorization.split('Bearer ')[1];
+
+    }else {
+        console.error('No token Found');
+        res.status(403).json({error: "unAuthorized"});
+    }
+
+    //check if token is valid
+    admin.auth().verifyIdToken(idToken)
+    .then((decodedToken)=>{
+        req.user = decodedToken;
+        // console.log(decodedToken);
+        return db.collection('users').where('userId', '==', req.user.uid).limit(1).get()
+    })
+    .then((data)=>{
+        req.user.handle = data.docs[0].data().handle;
+        return next();
+    })
+    .catch((err) => {
+        console.log('Error ', err);
+        res.status(403).json({"error": err})
+    });
+    
+
+
+
+}
 
 /*
 * Get Screams
 */
 
-app.get('/screams', (req, res) => {
+app.get('/screams',  (req, res) => {
     db.collection('screams').orderBy('createdAt', 'desc').get().then((snapshot) => {
         let screams = [];
         snapshot.forEach((doc) => {
@@ -65,11 +100,11 @@ app.get('/screams', (req, res) => {
 * Post a new Scream
 */
 
-app.post('/screams', (req, res) => {
+app.post('/screams', FBauth , (req, res) => {
     let newScream = {
         body: req.body.body,
         createdAt:new Date().toISOString(),
-        userHandle: req.body.userHandle
+        userHandle: req.user.handle
     };
 
     db.collection('screams').add(newScream).then((doc)=>{
@@ -139,6 +174,7 @@ app.post('/signup', (req, res)=> {
     }
 
     let token, userId;
+    let noImg ="noImg.webp";
 
     //check if the username or userHandle existed before
     db.doc(`/users/${newUser.handle}`).get().then((doc)=>{
@@ -163,6 +199,7 @@ app.post('/signup', (req, res)=> {
             email: newUser.email,
             createdAt: new Date().toISOString(),
             userId:userId,
+            imageUrl: `https://firebasestorage.googleapis.com/v0/b/${process.env.STORAGE_BUCKET}/o/${noImg}?alt=media`
 
         }
 
@@ -176,15 +213,145 @@ app.post('/signup', (req, res)=> {
     .catch((err) => {
         console.log('Error getting documents', err);
         if(err.code === "auth/email-already-in-use"){
-            return res.status(400).json({email: "this email has been taken"});
+             res.status(400).json({email: "this email has been taken"});
         }else{
-        res.status(500).json({"error": err})
+         res.status(500).json({"error": err})
         }
     });
 
    
 });
- 
+
+
+/*
+* Login Route
+*/
+app.post('/login', (req, res)=> {
+    //extracting request data
+    const newUser = {
+        email: req.body.email,
+        password: req.body.password,
+        
+    }
+
+    //validate request data
+    let errors = {};
+    if(isEmpty(newUser.email)){
+        errors.email = "must not be empty"
+    }
+    else if (!isEmail(newUser.email)){
+        errors.email = "Email should be Valid!"
+    }
+   if (isEmpty(newUser.password)){
+        errors.password = "Must not be empty"
+    }
+    
+    //break if there is an error
+    if (Object.keys(errors).length > 0){ 
+        return res.status(400).json(errors);
+    }
+
+    firebase.auth().signInWithEmailAndPassword(newUser.email, newUser.password)
+    .then((data)=>{
+        return data.user.getIdToken(); 
+    })
+    .then((token)=>{
+        res.json({token})
+    })
+    .catch((err) => {
+        console.log('Error', err);
+        if(err.code === "auth/wrong-password"){
+             res.status(403).json({general: "wrong credentials, try again"});
+        }else{
+         res.status(500).json({"error": err})
+        }
+    });
+
+});
+
+
+/*
+* Add user Details
+*/
+app.post('/user', FBauth, (req, res) =>{
+     let userDetails = {};
+
+     if(!isEmpty(req.body.bio.trim())){
+         userDetails.bio = req.body.bio.trim();
+     }
+
+     if(!isEmpty(req.body.website.trim())){
+         if(req.body.website.trim().substring(0,4) !== 'http' ){
+             userDetails.website = 'http://'+ req.body.website.trim();
+         } else{
+             userDetails.website = req.body.website.trim();
+         }
+     }
+
+     if(!isEmpty(req.body.location)){
+         userDetails.location = req.body.location;
+     }
+
+});
+
+
+/*
+* Upload user Image Route
+*/
+app.post('/user/image', FBauth , (req, res)=>{
+    const BusBoy = require('busboy');
+    const path = require('path');
+    const os = require('os');
+    const fs = require('fs');
+
+    const busboy = new BusBoy({ headers: req.headers });
+
+    let imageToBeUploaded = {};
+    let imageFileName;
+
+    busboy.on('file', (fieldname, file, filename, encoding, mimetype)=>{
+        // console.log(fieldname, file, filename, encoding, mimetype);
+        if (mimetype !== 'image/jpeg' && mimetype !== 'image/png') {
+            return res.status(400).json({ error: 'Wrong file type submitted' });
+          }
+
+        const imageExtension = filename.split('.')[filename.split('.').length - 1];
+        imageFileName = Math.round(Math.random() * 1000000000000).toString().trim()+'.'+imageExtension;
+        const filepath = path.join(os.tmpdir(), imageFileName);
+        imageToBeUploaded = { filepath, mimetype };
+        file.pipe(fs.createWriteStream(filepath));
+    })
+
+
+    busboy.on('finish', () => {
+        admin
+          .storage()
+          .bucket()
+          .upload(imageToBeUploaded.filepath, {
+            resumable: false,
+            metadata: {
+              metadata: {
+                contentType: imageToBeUploaded.mimetype
+              }
+            }
+          })
+          .then(() => {
+            const imageUrl = 'https://firebasestorage.googleapis.com/v0/b/'+process.env.STORAGE_BUCKET+'/o/'+imageFileName+'?alt=media';
+            return db.doc(`/users/${req.user.handle}`).update({ imageUrl });
+          })
+          .then(() => {
+            return res.json({ message: 'image uploaded successfully' });
+          })
+          .catch((err) => {
+            console.error(err);
+            return res.status(500).json({ error: 'something went wrong' });
+          });
+      });
+
+      busboy.end(req.rawBody);
+
+
+})
 
 /*
 * Exporting /api route to handle other back-end functions
